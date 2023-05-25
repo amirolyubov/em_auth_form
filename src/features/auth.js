c18813d8dc7000d58262e13494e46f3ba5111249
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-05-25 19:47:42 +0400

    fix dfault auth state

--- src/features/auth.js
+++ src/features/auth.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
     name: "auth",
     initialState: {
-        isAuthentificated: true,
+        isAuthentificated: false,
     },
     reducers: {
         setAuthentificated: (state, action) => {
